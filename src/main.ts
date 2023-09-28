c30b85ebf1c10ab53483225a28c6edff4112c12f
shawna <shawnalehman@gmail.com>
2023-09-28 08:24:31 +0300

    Updates/main.ts: added comments

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,21 @@ import './base.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
 import App from './App.vue'
 import BaseIcon from '@/components/atoms/BaseIcon.vue'
 
+// Create App
 const app = createApp(App)
+
+// Create state store
 const pinia = createPinia()
+
+// Attach state store to app
 app.use(pinia)
 
+// Globally register BaseIcon on App for ease of access
 app.component('BaseIcon', BaseIcon)
 
+// Mount App
 app.mount('#app')
